cd6a79f3c4f283b48925d9c17ba68bcc5d4f55b7
FaridLulaniya <faridlulaniya@gmail.com>
2024-06-26 10:15:21 +0530

    Looper

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char current_char = 'a';
+    
+    do {
+        printf("%c , ", current_char);
+        current_char += 4; 
+    } while (current_char <= 'z');
+    
+    printf("\n");
+    
+    return 0;
+}
